0ab9edfc374367a815e1b4c77bd66ae6a0611037
NerdyLucifer <ashwat.kumarsingh.met20@itbhu.ac.in>
2021-10-08 11:06:55 +0530

    fixed load more

--- pages/events.js
+++ pages/events.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Container, Row, Col, Button, Card} from "react-bootstrap";
+import { Container, Row, Col, Button, Card } from "react-bootstrap";
 import Footer from "../components/Footer";
 import Poster from "../public/esummit_poster.png";
 import Image from "next/image";
@@ -7,9 +7,10 @@ import casex1 from "../public/casex1.png";
 import casex2 from "../public/casex2.png";
 import casex3 from "../public/casex3.png";
 import Nav from "../components/navbar/NavLayout";
-import { useState} from "react";
+import { useState } from "react";
 export default function Events() {
   const [loadMore, setLoadMore] = useState(false);
+  const [loadMore1, setLoadMore1] = useState(false);
   return (
     <>
       <Head>
@@ -141,13 +142,15 @@ export default function Events() {
                 </Card.Body>
               </Card>
             </Row>
-            <Button
-              variant="warning"
-              style={{ width: "150px", margin: "auto"}}
-              onClick={() => setLoadMore(!loadMore)}
-            >
-              Load More
-            </Button>
+            {!loadMore && (
+              <Button
+                variant="warning"
+                style={{ width: "150px", margin: "auto" }}
+                onClick={() => setLoadMore(!loadMore)}
+              >
+                Load More
+              </Button>
+            )}
             {loadMore && (
               <Row className="card-container">
                 <Card className="card" style={{ height: "fit-content" }}>
@@ -218,8 +221,87 @@ export default function Events() {
                 </Card>
               </Row>
             )}
+            {loadMore && !loadMore1 && (
+              <Button
+                variant="warning"
+                style={{ width: "150px", margin: "auto" }}
+                onClick={() => setLoadMore1(true)}
+              >
+                Load More
+              </Button>
+            )}
+            {loadMore1 && (
+              <Row className="card-container">
+                <Card className="card" style={{ height: "fit-content" }}>
+                  <Card.Body style={{ padding: "25px 15px" }}>
+                    <Image src={casex1} height={320} width={290}></Image>
+                    <Card.Title style={{ margin: "10px auto" }}>
+                      CaseX
+                    </Card.Title>
+                    <Card.Text style={{ height: "15px" }}>
+                      3rd-12th Sept
+                    </Card.Text>
+                    <Button
+                      variant="danger"
+                      style={{
+                        backgroundColor: "#FA8231",
+                        border: "#FA8231",
+                        margin: "8px auto",
+                      }}
+                      className="card-button"
+                    >
+                      Know More! &rarr;
+                    </Button>
+                  </Card.Body>
+                </Card>
+                <Card className="card" style={{ height: "fit-content" }}>
+                  <Card.Body style={{ padding: "25px 15px" }}>
+                    <Image src={casex2} height={320} width={290}></Image>
+                    <Card.Title style={{ margin: "10px auto" }}>
+                      Idea Validation Bootcamp
+                    </Card.Title>
+                    <Card.Text style={{ height: "15px" }}>
+                      3rd-12th Sept
+                    </Card.Text>
+                    <Button
+                      variant="danger"
+                      style={{
+                        backgroundColor: "#FA8231",
+                        border: "#FA8231",
+                        margin: "8px auto",
+                      }}
+                      className="card-button"
+                    >
+                      Know More! &rarr;
+                    </Button>
+                  </Card.Body>
+                </Card>
+                <Card className="card" style={{ height: "fit-content" }}>
+                  <Card.Body style={{ padding: "25px 15px" }}>
+                    <Image src={casex3} height={320} width={290}></Image>
+                    <Card.Title style={{ margin: "10px auto" }}>
+                      Design Rush
+                    </Card.Title>
+                    <Card.Text style={{ height: "15px" }}>
+                      3rd-12th Sept
+                    </Card.Text>
+                    <Button
+                      variant="danger"
+                      style={{
+                        backgroundColor: "#FA8231",
+                        border: "#FA8231",
+                        margin: "8px auto",
+                      }}
+                      className="card-button"
+                    >
+                      Know More! &rarr;
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Row>
+            )}
           </Row>
-          <Footer/>
+          <Footer />
         </Container>
       </div>
     </>
