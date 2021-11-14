36feba397a789a307d0a2952a130897c90ff9e1f
NerdyLucifer <ashwat.kumarsingh.met20@itbhu.ac.in>
2021-11-14 20:15:40 +0530

    changes

--- components/Gallery.js
+++ components/Gallery.js
@@ -2,58 +2,81 @@ import { ReactPhotoCollage } from "react-photo-collage";
 import React, { useState, useCallback } from "react";
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
-
+const photos = [
+  {
+    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    width: 4,
+    height: 3,
+    title:
+      "i am very good ankjba absbbas iadib hiasgad agyiaga aguyaguya hgiuagia agyidgd gygduyd guygduav agyuaguva",
+  },
+  {
+    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    width: 1,
+    height: 1,
+  },
+  {
+    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    width: 3,
+    height: 4,
+  },
+  {
+    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    width: 3,
+    height: 4,
+  },
+  {
+    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    width: 3,
+    height: 4,
+  },
+  {
+    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    width: 3,
+    height: 4,
+  },
+  {
+    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3,
+  },
+];
 export const Esummit = () => {
-  const photos = [
-    {
-      src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
-      width: 4,
-      height: 3,
-      title:
-        "i am very good ankjba absbbas iadib hiasgad agyiaga aguyaguya hgiuagia agyidgd gygduyd guygduav agyuaguva",
-    },
-    {
-      src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
-      width: 1,
-      height: 1,
-    },
-    {
-      src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
-      width: 3,
-      height: 4,
-    },
-    {
-      src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
-      width: 3,
-      height: 4,
-    },
-    {
-      src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
-      width: 3,
-      height: 4,
-    },
-    {
-      src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
-      width: 4,
-      height: 3,
-    },
-    {
-      src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
-      width: 3,
-      height: 4,
-    },
-    {
-      src: "https://source.unsplash.com/PpOHJezOalU/800x599",
-      width: 4,
-      height: 3,
-    },
-    {
-      src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
-      width: 4,
-      height: 3,
-    },
-  ];
-
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
@@ -69,7 +92,7 @@ export const Esummit = () => {
 
   return (
     <div>
-      <Gallery photos={photos} onClick={openLightbox} />
+      <Gallery photos={photos.slice(0, 8)} onClick={openLightbox} />
       <ModalGateway>
         {viewerIsOpen ? (
           <Modal onClose={closeLightbox}>
@@ -88,53 +111,104 @@ export const Esummit = () => {
   );
 };
 export const FoundersSpeak = () => {
-  const setting = {
-    width: "900px",
-    height: ["400px", "170px"],
-    layout: [2, 3],
-    photos: [
-      { source: "https://source.unsplash.com/random" },
-      { source: "https://source.unsplash.com/random" },
-      { source: "https://source.unsplash.com/random" },
-      { source: "https://source.unsplash.com/random" },
-      { source: "https://source.unsplash.com/random" },
-      { source: "https://source.unsplash.com/random" },
-    ],
-    showNumOfRemainingPhotos: true,
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
   };
-  return <ReactPhotoCollage {...setting} />;
+
+  return (
+    <div>
+      <Gallery photos={photos.slice(0, 8)} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map((x) => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
+  );
 };
 export const Workshops = () => {
-  const setting = {
-    width: "900px",
-    height: ["400px", "170px"],
-    layout: [2, 3],
-    photos: [
-      { source: "https://source.unsplash.com/random" },
-      { source: "https://source.unsplash.com/random" },
-      { source: "https://source.unsplash.com/random" },
-      { source: "https://source.unsplash.com/random" },
-      { source: "https://source.unsplash.com/random" },
-      { source: "https://source.unsplash.com/random" },
-    ],
-    showNumOfRemainingPhotos: true,
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
   };
-  return <ReactPhotoCollage {...setting} />;
+
+  return (
+    <div>
+      <Gallery photos={photos.slice(0, 8)} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map((x) => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
+  );
 };
 export const GuestLectures = () => {
-  const setting = {
-    width: "900px",
-    height: ["400px", "170px"],
-    layout: [2, 3],
-    photos: [
-      { source: "https://source.unsplash.com/random" },
-      { source: "https://source.unsplash.com/random" },
-      { source: "https://source.unsplash.com/random" },
-      { source: "https://source.unsplash.com/random" },
-      { source: "https://source.unsplash.com/random" },
-      { source: "https://source.unsplash.com/random" },
-    ],
-    showNumOfRemainingPhotos: true,
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
   };
-  return <ReactPhotoCollage {...setting} />;
+
+  return (
+    <div>
+      <Gallery photos={photos.slice(0, 8)} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map((x) => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
+  );
 };
