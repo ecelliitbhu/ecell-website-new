093f0a2f23f02a2ad0c75a9cd42dd86be4b4ed74
NerdyLucifer <ashwat.kumarsingh.met20@itbhu.ac.in>
2021-12-29 03:24:05 +0530

    made startup page

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,7 +1,26 @@
 import React from "react";
-
+import Head from "next/head";
 const ContactForm = () => {
-  return <></>;
+  return (
+    <>
+      <form className="cf">
+        <div className="half left cf">
+          <input type="text" id="input-name" placeholder="Name" />
+          <input type="email" id="input-email" placeholder="Email address" />
+          <input type="text" id="input-subject" placeholder="Subject" />
+        </div>
+        <div className="half right cf">
+          <textarea
+            name="message"
+            type="text"
+            id="input-message"
+            placeholder="Message"
+          ></textarea>
+        </div>
+        <input type="submit" value="Submit" id="input-submit" />
+      </form>
+    </>
+  );
 };
 
 export default ContactForm;
---
+++ components/startups/Startup.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+const Startup = () => {
+  const [modalShow, setModalShow] = React.useState(false);
+  return (
+    <>
+      <div className="startup-item" onClick={() => setModalShow(true)}></div>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
+};
+
+export default Startup;
