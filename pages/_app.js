280b6ec41b41f2a5950ada6e62064d3221f0527b
apoorve <apoorve.shukla.cse20@itbhu.ac.in>
2022-01-25 21:57:08 +0530

    Added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,16 @@ import "../styles/globals.scss";
 import Layout from "../components/Layout";
 import SSRProvider from "react-bootstrap/SSRProvider";
 import { AuthProvider } from "../context/auth";
+import Head from "next/head";
+
 function MyApp({ Component, pageProps }) {
   return (
     <SSRProvider>
       <AuthProvider>
         <Layout>
+          <Head>
+            <link rel="shortcut icon" href="/favicon.ico" />
+          </Head>
           <Component {...pageProps} />
         </Layout>
       </AuthProvider>
