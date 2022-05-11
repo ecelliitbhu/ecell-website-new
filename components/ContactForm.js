f41ca8d709fdb053df604ae4b57736dc360f9999
NerdyLucifer <ashwat.kumarsingh.met20@itbhu.ac.in>
2022-05-11 12:28:04 +0530

    team expansion form

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,33 +1,90 @@
 import React from "react";
-import Head from "next/head";
+import { firebaseDB } from "../lib/firebase";
+import { ref, push} from "firebase/database";
+import { useState } from "react";
 const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState();
+  const [startup, setStartup] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const db = firebaseDB;
+    push(ref(db, "contactUs/"), {
+      name: name,
+      email: email,
+      contact_number: number,
+      startup: startup,
+      subject: subject,
+      message: message,
+    })
+      .then(() => alert("Form submitted successfully"))
+      .catch((error) => alert(error.message));
+      setName("");
+      setEmail("");
+      setNumber("");
+      setStartup("");
+      setSubject("");
+      setMessage("");
+  };
   return (
     <>
-      <form className="cf">
+      <form className="cf contact-form" onSubmit={handleSubmit}>
         <div className="half left cf">
-          <input type="text" id="input-name" placeholder="Name" />
-          <input type="email" id="input-email" placeholder="Email address" />
-          <input type="text" id="input-number" placeholder="Contact Number" />
           <input
             type="text"
-            id="input-startup"
+            id="contact-name"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <input
+            type="email"
+            id="contact-email"
+            placeholder="Email address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="text"
+            id="contact-number"
+            placeholder="Contact Number"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+            required
+          />
+          <input
+            type="text"
+            id="contact-startup/company"
             placeholder="Name of startup/company"
+            value={startup}
+            onChange={(e) => setStartup(e.target.value)}
           />
         </div>
         <div className="half right cf">
           <textarea
             name="subject"
             type="text"
-            id="input-subject"
+            id="contact-subject"
             placeholder="Subject"
-            style={{height:"20%"}}
+            style={{ height: "20%" }}
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+            required
           ></textarea>
           <textarea
             name="message"
             type="text"
-            id="input-message"
+            id="contact-message"
             placeholder="Message"
-            style={{height:"175px"}}
+            style={{ height: "175px" }}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
           ></textarea>
         </div>
         <input type="submit" value="Submit" id="input-submit" />
