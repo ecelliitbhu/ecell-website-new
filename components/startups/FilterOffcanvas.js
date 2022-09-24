edd66e54d66454bbb8ed16fba18104e415294ca9
NerdyLucifer <78917642+iamaks287@users.noreply.github.com>
2022-09-24 16:40:43 +0530

    startup directory filter

--- components/startups/FilterOffcanvas.js
+++ components/startups/FilterOffcanvas.js
@@ -4,7 +4,8 @@ import { Col, Offcanvas, Row } from "react-bootstrap";
 import { FaFilter } from "react-icons/fa";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 import CloseIcon from "@mui/icons-material/Close";
-const FilterOffcanvas = () => {
+import Filter from "./Filter";
+const FilterOffcanvas = ({ domainFiltersList, setDomainFiltersList }) => {
   const [state, setState] = React.useState({
     top: false,
     left: false,
@@ -45,103 +46,11 @@ const FilterOffcanvas = () => {
               }}
               onClick={toggleDrawer("left", false)}
             ></IoIosCloseCircleOutline>
-
-            <h2>
-              Filter out the results{" "}
-              <FaFilter className="filter-icon"></FaFilter>
-            </h2>
           </Row>
-          <Row class="filter-subcontainer">
-            <h3>Domains</h3>
-            <ul>
-              <li>
-                <input
-                  type="checkbox"
-                  id="agritech"
-                  name="agritech"
-                  value="agritech"
-                />
-                <label htmlFor="agritech"> Agritech</label>
-              </li>
-              <li>
-                <input
-                  type="checkbox"
-                  id="agritech"
-                  name="agritech"
-                  value="agritech"
-                />
-                <label htmlFor="agritech"> Agritech</label>
-              </li>
-              <li>
-                <input
-                  type="checkbox"
-                  id="agritech"
-                  name="agritech"
-                  value="agritech"
-                />
-                <label htmlFor="agritech"> Agritech</label>
-              </li>
-              <li>
-                <input
-                  type="checkbox"
-                  id="agritech"
-                  name="agritech"
-                  value="agritech"
-                />
-                <label htmlFor="agritech"> Agritech</label>
-              </li>
-              <li>
-                <input
-                  type="checkbox"
-                  id="agritech"
-                  name="agritech"
-                  value="agritech"
-                />
-                <label htmlFor="agritech"> Agritech</label>
-              </li>
-            </ul>
-          </Row>
-          <Row class="filter-subcontainer">
-            <h3>Founded in</h3>
-            <ul>
-              <li>
-                <input
-                  type="checkbox"
-                  id="2019-2022"
-                  name="2019-2022"
-                  value="2019-2022"
-                />
-                <label htmlFor="2019-2022"> 2019-2022</label>
-              </li>
-              <li>
-                <input
-                  type="checkbox"
-                  id="2015-2019"
-                  name="2015-2019"
-                  value="2015-2019"
-                />
-                <label htmlFor="2015-2019"> 2015-2019</label>
-              </li>
-              <li>
-                <input
-                  type="checkbox"
-                  id="2010-2015"
-                  name="2010-2015"
-                  value="2010-2015"
-                />
-                <label htmlFor="2010-2015"> 2010-2015</label>
-              </li>
-              <li>
-                <input
-                  type="checkbox"
-                  id="Before 2010"
-                  name="Before 2010"
-                  value="Before 2010"
-                />
-                <label htmlFor="Before 2010"> Before 2010</label>
-              </li>
-            </ul>
-          </Row>
+          <Filter
+            domainFiltersList={domainFiltersList}
+            setDomainFiltersList={setDomainFiltersList}
+          />
         </Col>
       </Drawer>
     </>
