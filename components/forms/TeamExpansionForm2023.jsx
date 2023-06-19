dbddc7fd6eefba982af5f4fdde4d3907f887445b
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-06-20 00:27:13 +0530

    styled the form

--- components/forms/TeamExpansionForm2023.jsx
+++ components/forms/TeamExpansionForm2023.jsx
@@ -69,159 +69,172 @@ export default function TeamExpansionForm2023() {
     console.log(data);
   };
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <Form.Group className="mb-3" controlId="FullName">
-        <Form.Label>
-          Full Name<span style={{ color: "red" }}> *</span>
-        </Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Bill Gates"
-          {...register("fullname", { required: true })}
-        />
-        {errors.fullname && (
-          <span style={{ color: "red" }}>Please provide your full name</span>
-        )}
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="email">
-        <Form.Label>
-          Email address<span style={{ color: "red" }}> *</span>
-        </Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="name@itbhu.ac.in"
-          {...register("email", { required: true })}
-        />
-        {errors.email && (
-          <span style={{ color: "red" }}>
-            Please provide your institute email
-          </span>
-        )}
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="PhoneNumber">
-        <Form.Label>
-          Phone Number(WhatsApp)<span style={{ color: "red" }}> *</span>
-        </Form.Label>
-        <Form.Control
-          type="number"
-          placeholder="9999888877"
-          {...register("phoneNumber", { required: true })}
-        />
-        {errors.phoneNumber && (
-          <span style={{ color: "red" }}>Please provide your phone number</span>
-        )}
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="Course">
-        <Form.Label>
-          Select your course <span style={{ color: "red" }}> *</span>
-        </Form.Label>
-        <Form.Select
-          aria-label="Default select example"
-          {...register("course", { required: true })}
-        >
-          <option>Open this select menu</option>
-          <option value="BTech">BTech</option>
-          <option value="BArch">BArch</option>
-          <option value="IDD">IDD</option>
-          <option value="MTech">MTech</option>
-          <option value="PhD">PhD</option>
-        </Form.Select>
-        {errors.course && (
-          <span style={{ color: "red" }}>Please select your course</span>
-        )}
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="Branch">
-        <Form.Label>
-          Select your branch <span style={{ color: "red" }}> *</span>
-        </Form.Label>
-        <Form.Select
-          aria-label="Default select example"
-          {...register("branch", { required: true })}
-        >
-          <option>Open this select menu</option>
-          {BRANCHES.map((branch) => (
-            <option key={branch} value={branch}>
-              {branch}
-            </option>
-          ))}
-        </Form.Select>
-        {errors.branch && (
-          <span style={{ color: "red" }}>Please select your branch</span>
-        )}
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="Verticals">
-        <Form.Label>Select interested verticals</Form.Label>
-        {VERTICALS.map((vertical) => (
-          <Form.Check
-            key={vertical}
-            type="checkbox"
-            label={vertical}
-            value={vertical}
-            {...register("interestedVerticals")}
-          />
-        ))}
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="Commitment">
-        <Form.Label>Commitment (hours per week)</Form.Label>
-        <Form.Range
-          {...register("commitmentHours")}
-          min={1}
-          max={20}
-          defaultValue={5}
-          onChange={handleCommitmentChange}
-        />
-        <div>Selected Range: {commitmentValue}</div>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="Reason">
-        <Form.Label>
-          Why do you wish to join E-Cell IIT BHU and the above selected
-          teams(s)? <span style={{ color: "red" }}> *</span>
-        </Form.Label>
-        <Form.Control
-          as="textarea"
-          placeholder="Enter your reason here..."
-          {...register("reason", { required: true })}
-        />
-        {errors.reason && (
-          <span style={{ color: "red" }}>
-            Please provide your reason to join E-Cell
-          </span>
-        )}
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="PreviousExperiences">
-        <Form.Label>
-          Previous experiences in student organization/management (please
-          mention in points)
-        </Form.Label>
-        <Form.Control
-          as="textarea"
-          placeholder="Enter your previous experiences here..."
-          {...register("previousExperiences")}
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="NonAcademicEngagements">
-        <Form.Label>Any non-academic engagements?</Form.Label>
-        <Form.Control
-          as="textarea"
-          placeholder="Enter your non-academic engagements here..."
-          {...register("nonAcademicEngagements")}
-        />
-      </Form.Group>
+    <div className={"form-card"}>
+      <div className={"form-card-content"}>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <div className={"name-email"}>
+            <Form.Group className="mb-3" controlId="FullName">
+              <Form.Label style={{ fontSize: "1.2rem" }}>
+                Full Name<span style={{ color: "red" }}> *</span>
+              </Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Bill Gates"
+                {...register("fullname", { required: true })}
+              />
+              {errors.fullname && (
+                <span style={{ color: "red" }}>
+                  Please provide your full name
+                </span>
+              )}
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="email">
+              <Form.Label>
+                Email address<span style={{ color: "red" }}> *</span>
+              </Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@itbhu.ac.in"
+                {...register("email", { required: true })}
+              />
+              {errors.email && (
+                <span style={{ color: "red" }}>
+                  Please provide your institute email
+                </span>
+              )}
+            </Form.Group>
+          </div>
+          <Form.Group className="mb-3" controlId="PhoneNumber">
+            <Form.Label>
+              Phone Number(WhatsApp)<span style={{ color: "red" }}> *</span>
+            </Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="9999888877"
+              {...register("phoneNumber", { required: true })}
+            />
+            {errors.phoneNumber && (
+              <span style={{ color: "red" }}>
+                Please provide your phone number
+              </span>
+            )}
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="Course">
+            <Form.Label>
+              Select your course <span style={{ color: "red" }}> *</span>
+            </Form.Label>
+            <Form.Select
+              aria-label="Default select example"
+              {...register("course", { required: true })}
+            >
+              <option>Open this select menu</option>
+              <option value="BTech">BTech</option>
+              <option value="BArch">BArch</option>
+              <option value="IDD">IDD</option>
+              <option value="MTech">MTech</option>
+              <option value="PhD">PhD</option>
+            </Form.Select>
+            {errors.course && (
+              <span style={{ color: "red" }}>Please select your course</span>
+            )}
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="Branch">
+            <Form.Label>
+              Select your branch <span style={{ color: "red" }}> *</span>
+            </Form.Label>
+            <Form.Select
+              aria-label="Default select example"
+              {...register("branch", { required: true })}
+            >
+              <option>Open this select menu</option>
+              {BRANCHES.map((branch) => (
+                <option key={branch} value={branch}>
+                  {branch}
+                </option>
+              ))}
+            </Form.Select>
+            {errors.branch && (
+              <span style={{ color: "red" }}>Please select your branch</span>
+            )}
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="Verticals">
+            <Form.Label>Select interested verticals</Form.Label>
+            <div style={{ paddingLeft: ".825rem" }}>
+              {VERTICALS.map((vertical) => (
+                <Form.Check
+                  key={vertical}
+                  className={"form-card-description"}
+                  type="checkbox"
+                  label={vertical}
+                  value={vertical}
+                  {...register("interestedVerticals")}
+                />
+              ))}
+            </div>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="Commitment">
+            <Form.Label>Commitment (hours per week)</Form.Label>
+            <Form.Range
+              {...register("commitmentHours")}
+              min={1}
+              max={20}
+              defaultValue={5}
+              onChange={handleCommitmentChange}
+            />
+            <div>Selected Range: {commitmentValue}</div>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="Reason">
+            <Form.Label>
+              Why do you wish to join E-Cell IIT BHU and the above selected
+              teams(s)? <span style={{ color: "red" }}> *</span>
+            </Form.Label>
+            <Form.Control
+              as="textarea"
+              placeholder="Enter your reason here..."
+              {...register("reason", { required: true })}
+            />
+            {errors.reason && (
+              <span style={{ color: "red" }}>
+                Please provide your reason to join E-Cell
+              </span>
+            )}
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="PreviousExperiences">
+            <Form.Label>
+              Previous experiences in student organization/management (please
+              mention in points)
+            </Form.Label>
+            <Form.Control
+              as="textarea"
+              placeholder="Enter your previous experiences here..."
+              {...register("previousExperiences")}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="NonAcademicEngagements">
+            <Form.Label>Any non-academic engagements?</Form.Label>
+            <Form.Control
+              as="textarea"
+              placeholder="Enter your non-academic engagements here..."
+              {...register("nonAcademicEngagements")}
+            />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="OtherPoints">
-        <Form.Label>Any other point(s) you wish to mention</Form.Label>
-        <Form.Control
-          as="textarea"
-          placeholder="Enter any other points here..."
-          {...register("otherPoints")}
-        />
-      </Form.Group>
-      <div className="d-grid gap-2">
-        <Button variant="primary" type={"submit"} size="lg">
-          Submit
-        </Button>
+          <Form.Group className="mb-3" controlId="OtherPoints">
+            <Form.Label>Any other point(s) you wish to mention</Form.Label>
+            <Form.Control
+              as="textarea"
+              placeholder="Enter any other points here..."
+              {...register("otherPoints")}
+            />
+          </Form.Group>
+          <div className="d-grid gap-2">
+            <Button variant="primary" type={"submit"} size="lg">
+              Submit
+            </Button>
+          </div>
+          <DevTool control={control} placement="top-right" />
+        </Form>
       </div>
-      <DevTool control={control} placement="top-right" />
-    </Form>
+    </div>
   );
 }
