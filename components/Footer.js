236a29fbe0b44cb8d7a02239adf89f54a20fb737
Arun <arun.kumar.cd.civ21@itbhu.ac.in>
2023-08-27 19:51:13 +0530

    added sticky bar

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,7 @@
 import { Row, Col } from "react-bootstrap";
 import Link from "next/link";
+import { useState } from "react";
+// import 'boxicons'
 
 import {
   FaInstagram,
@@ -14,6 +16,12 @@ import { MdLocationOn } from "react-icons/md";
 import { BsFillTelephoneOutboundFill } from "react-icons/bs";
 
 const Footer = () => {
+
+  const [stickyBar, setStickyBar] = useState(false);
+
+  const showSticky = () => {
+    setStickyBar(!stickyBar);
+  }
   return (
     <>
       <footer className="footer">
@@ -137,7 +145,10 @@ const Footer = () => {
             ></iframe>
           </Col>
         </Row>
-        <Row className="social">
+        <div className={stickyBar ? "show-sticky-bar" : "hide-sticky-bar"} style={{ position: "fixed" }}>
+          {/* <i className='bx bx-chevron-right'></i> */}
+          {/* <img className="bar-slider2" onClick={showSticky} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAYAAADhAJiYAAAAAXNSR0IArs4c6QAAAO9JREFUWEftlksKwjAQQF+PIHoHUXcuFcSj+9u7cOPClRtBUQQvIIEGQqkOnTQ0ynQ7DHl980kKMvuKzHgwIKkiZsgMSQak+N/30Ai4A1fJxKd4m4aGwA54AUvgrIFqC2gMbIB+CbEHpl0BeTODEuAGzIBTF0ATYAWEMAvgqIFxOTElczBboBeYiYKJAUoCowWqwjyAeUyZwvI2LVkdjBvxg7Znqnk/D+R+KKmlpoa84WR9pAWqM+UWYmdjH5rKZjF6qKyujtDUOpfLtWoqi+eHh3IPtCdw0S7KmCnTnvk1z4AkrWbIDEkGpLj1kGToDcnAMiW8WACSAAAAAElFTkSuQmCC" /> */}
+          <img className="bar-slider2" onClick={showSticky} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAYAAADhAJiYAAAAAXNSR0IArs4c6QAAAopJREFUWEfdmEtLVlEUhh8nYSHZoCZNsgaBCA0iiEgoaOCgXxDSxTQIiuxiDqIoSpBKEYJqUBSENnTkyIE4KoIimkROukAXslmCoATFC+fI4bAva28dfLThG513rff51r6cvU4TDTaaGoyH/waoEzgC7AW2FD8V+xcwD7wCJoCXqTOQUqFm4DRwHthmNPoIjAGPgCVLjBXoJHAb2GxJ6tCoaheA57F4C5BABmOJjM+vAUMhbQhIz54Cx41mVtk9oN8nDgE9LNaM1ShFdxO47grwAV0ERlMcMrRHgfF6nAtoE/AN2JBg8iVh55Vp5bEDWK76uIBUTi0+61Di/cBna0BFdxa4HwJaB/wEVCXL+F6B+WsJqGlU2bYQ0GFgypi4rIySauQAKW438Lb0rE/ZE6DHAFSHWQ2QzqWVJVIHegfsigC5YFYDNA10+Soks60BoOqaqctuBOIOAgc8z98DHT6gRWC9J9BXGcMMI1jnQQj8Blp9QAtAi8dBi1fbW2CpIwQkz40+oDlgZ8BNZ43uQi6o3Cn7ALT7gGYDc13G+CqVu+1ngEM+oLvAgGE+XFC5QMPAFR/QPuCFAUiSOlQu0B7gjQ9I59LXyNav8gpKa0oxOUCfihfsSk7Xy/USMGKsUrVSgkodZ4AH1SAXkK4d2k3qJqxD/3S7VVzozNcP6fuKTiHRI0ne7br0h66wd4DLSRZ2sfeyH+s6HgO9dh+TUn2arsjOEQNS0C3gqskqLNIuVDsV3DAWINmcAHRo5jaKWsDngMnYH7MCKY9a6VOAjgVrK613o9biM+BPDEbPU4DKfIrRW/9Ycf0sPzZoSvSx4UfxkUFt82sLROwcSs2xpvqcCq0pQD1ZwwH9A47GdiW/+wQnAAAAAElFTkSuQmCC" />
           <a
             href="https://www.linkedin.com/company/ecelliitbhu/"
             target="_blank"
@@ -181,7 +192,52 @@ const Footer = () => {
           >
             <FaDiscord className="social-icons"></FaDiscord>
           </a>
-        </Row>
+        </div>
+        {/* <Row className="social">
+          <a
+            href="https://www.linkedin.com/company/ecelliitbhu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin className="social-icons"></FaLinkedin>
+          </a>
+          <a
+            href="https://www.instagram.com/ecelliitbhu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram className="social-icons"></FaInstagram>
+          </a>
+          <a
+            href="https://www.facebook.com/ecelliitvaranasi"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebook className="social-icons"></FaFacebook>
+          </a>
+
+          <a
+            href="https://twitter.com/ecelliitbhu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitter className="social-icons"></FaTwitter>
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCUme5nNmSKY1GiUBUhlAnOQ"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaYoutube className="social-icons"></FaYoutube>
+          </a>
+          <a
+            href="https://discord.com/invite/EPm5mfbCKP"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaDiscord className="social-icons"></FaDiscord>
+          </a>
+        </Row> */}
       </footer>
     </>
   );
