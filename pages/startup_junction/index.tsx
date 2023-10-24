8bef64a0b08b2f06464e01933aae46c6aab342bc
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-10-24 11:53:58 +0530

    changes suggested

--- pages/startup_junction/index.tsx
+++ pages/startup_junction/index.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import styles from "./startup_junction.module.css";
 import Link from "next/link";
 import Head from "next/head";
+import { MapPin, CalendarDays } from "lucide-react";
 
 interface OwnProps {}
 
@@ -26,14 +27,26 @@ const StartupJunction: FunctionComponent<Props> = (props) => {
             <div className={styles.hero}>
               <div className="hero-text-box">
                 <h1 className={styles.headingPrimary}>Startup Junction </h1>
-                <h2 className={styles.headingSecondary}>
-                  Nurturing Innovation and Growth
-                </h2>
+                <div className={styles.venue}>
+                  <div>
+                    <CalendarDays />
+                    <span className={styles.heroDate}> 15th December 2023</span>
+                  </div>
+                  <div>
+                    <MapPin />
+                    <span className={styles.heroDate}>
+                      {" "}
+                      Delhi/NCR and Bengaluru
+                    </span>
+                  </div>
+                </div>
+
                 <p className={styles.heroDescription}>
-                  Startup Junction, an initiative by the Entrepreneurship Cell
-                  of IIT BHU, fosters our nation&apos;s startup ecosystem. We
-                  connect venture capitalists with promising startups, offering
-                  a dynamic platform for innovation, growth, and collaboration.
+                  Accelerate your startup&apos;s journey at Startup Junction,
+                  E-Cell IIT BHU&apos;s visionary initiative to connect
+                  promising startups with VCs, fostering innovation, growth, and
+                  collaboration. Register now for this incredible chance to
+                  propel your startup forward.
                 </p>
 
                 <Link
@@ -43,7 +56,7 @@ const StartupJunction: FunctionComponent<Props> = (props) => {
                   Register
                 </Link>
                 <Link
-                  href={"/"}
+                  href={"/startup_junction#about"}
                   className={`${styles.btn} ${styles.btnOutline} ${styles.marginRightBtn}`}
                 >
                   Learn more &darr;
@@ -58,7 +71,7 @@ const StartupJunction: FunctionComponent<Props> = (props) => {
                   className="hero-Image"
                 />
               </div>
-              <div className={styles.deliveredMeals}>
+              {/* <div className={styles.deliveredMeals}>
                 <div className={styles.deliveredImages}>
                   <Image
                     width={100}
@@ -100,10 +113,10 @@ const StartupJunction: FunctionComponent<Props> = (props) => {
                 <p className={styles.deliveredText}>
                   <span>250,000+</span> meals delivered last year!
                 </p>
-              </div>
+              </div> */}
             </div>
           </section>
-          <section className={styles.about}>
+          <section className={styles.about} id="about">
             <h1>About Startup Junction</h1>
             <p>
               Startup Junction, a pre-event of E-Summit&apos;24 is an initiative
@@ -111,8 +124,26 @@ const StartupJunction: FunctionComponent<Props> = (props) => {
               advancing the startup ecosystem in our country. Our vision is to
               connect venture capitalists with promising startups, providing a
               platform for innovation, growth, and collaboration.This one-day
-              event, will take place in Delhi/NCR and Bengaluru and serve as an
-              exclusive platform for visibility, mentorship, and funding.
+              event, will take place in Delhi/NCR and Bengaluru on 15th
+              Dec&apos;23, where the startups shortlisted after the due
+              diligence round would get to pitch in front our VC panel, and
+              serve as an exclusive platform for visibility, mentorship, and
+              funding.
+            </p>
+          </section>{" "}
+          <section className={styles.about}>
+            <h1>About E-Summit&apos;24</h1>
+            <p>
+              E-Summit IIT BHU is the annual flagship event of Entrepreneurship
+              Cell of IIT BHU. It stands as a testament to our commitment to
+              promoting entrepreneurship and fostering innovation. This event is
+              a celebration of the entrepreneurial spirit and serves as a
+              dynamic platform for startups, young innovators, students, and
+              industry leaders to come together, share knowledge, and inspire
+              innovation. The latest edition of this entrepreneurial
+              extravaganza, E-Summit&apos;24 is all set to take place from
+              February 2nd to February 4th, 2024, and promises to be an
+              enriching experience for all the participants.
             </p>
           </section>{" "}
           <section className={styles.about}>
@@ -130,15 +161,13 @@ const StartupJunction: FunctionComponent<Props> = (props) => {
           <section className={styles.about}>
             <h1>How to Register?</h1>
             <p>
-              To register, startups should complete the form provided below,
-              along with the submission of relevant documents. Our team will
-              then conduct the due diligence, verifying the provided information
-              and assessing each startup&apos;s potential. The shortlisted teams,
-              selected to pitch in front of the VC panel on the event day, will
-              be required to pay a fee of Rs. 599. This process ensures that the
-              most promising and committed startups have the opportunity to
-              connect with venture capitalists and elevate their entrepreneurial
-              journey.
+              To register, startups should click on the “Register” tab above and
+              complete the form provided, along with the submission of relevant
+              documents. Our team will then conduct the due diligence, verifying
+              the provided information and assessing each startup&apos;s potential.
+              The shortlisted teams, selected to pitch in front of the VC panel
+              on the event day, will be required to pay a moderate fee of Rs.
+              999 for the final presentation.
             </p>
           </section>
         </Container>
