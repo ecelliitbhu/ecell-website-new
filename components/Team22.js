dcdfe5e126663a8ca9acb74e5f1c44799a2f7d13
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2024-07-05 21:22:58 +0530

    Deleted static images and rendered it from cloud storage

--- components/Team22.js
+++ components/Team22.js
@@ -1,21 +1,13 @@
-import { Accordion, AccordionSummary } from "@mui/material";
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/legacy/image";
 import { GrMail } from "react-icons/gr";
-import { FaLinkedin, FaTwitter } from "react-icons/fa";
-import Amit from "../public/team/2022-2023/Amit.jpg";
-import Tanaya from "../public/team/2022-2023/Tanaya.jpg";
-import Tanya from "../public/team/2022-2023/Tanya.jpeg";
-import Parth from "../public/team/2022-2023/Parth.jpg";
-import Ashwat from "../public/team/2022-2023/Ashwat.jpg";
-import Divyansh from "../public/team/2022-2023/Divyansh.jpg";
-import Shreya from "../public/team/2022-2023/Shreya.jpg";
-import Vanshika from "../public/team/2022-2023/Vanshika.jpg";
+import { FaLinkedin } from "react-icons/fa";
+import { listAll, getDownloadURL, ref } from "firebase/storage";
+import { StorageDB } from "@/lib/firebase";
 const presidents = [
   {
     name: "AMIT KUMAR BAHETI",
-    image: Amit,
+    image: "Amit.jpg",
     post: "VICE PRESIDENT E-CELL",
     email:
       "https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=amit.krbaheti.eee19@iitbhu.ac.in",
@@ -24,7 +16,7 @@ const presidents = [
   },
   {
     name: "TANAYA MUJUMDAR",
-    image: Tanaya,
+    image: "Tanaya.jpg",
     post: "ASSOCIATE VICE PRESIDENT E-CELL",
     email: "mailto:mujumdartanaya@gmail.com",
     linkedin: "https://www.linkedin.com/in/tanaya-mujumdar-80aa17207",
@@ -34,7 +26,7 @@ const presidents = [
 const verticalHeads = [
   {
     name: "TANYA GUPTA",
-    image: Tanya,
+    image: "Tanya.jpeg",
     position: "BRANDING HEAD",
     email: "mailto:tanya.gupta.che20@itbhu.ac.in",
     linkedin: "https://www.linkedin.com/in/tanya-gupta-215942214",
@@ -43,7 +35,7 @@ const verticalHeads = [
   },
   {
     name: "PARTH GUPTA",
-    image: Parth,
+    image: "Parth.jpg",
     position: "SAP HEAD",
     email: "mailto:parthsanjeev.gupta.che20@itbhu.ac.in",
     linkedin: "https://www.linkedin.com/in/parthgupta03/",
@@ -51,7 +43,7 @@ const verticalHeads = [
   },
   {
     name: "ASHWAT KUMAR SINGH",
-    image: Ashwat,
+    image: "Ashwat.jpg",
     position: "TECH HEAD",
     email: "mailto:ashwat.kumarsingh.met20@itbhu.ac.in",
     linkedin: "https://www.linkedin.com/in/ashwat-singh/",
@@ -59,7 +51,7 @@ const verticalHeads = [
   },
   {
     name: "SHREYA JAIN",
-    image: Shreya,
+    image: "Shreya.jpg",
     position: "EVENTS HEAD",
     email: "mailto:shreya.jain.cd.eee20@iitbhu.ac.in",
     linkedin: "https://www.linkedin.com/in/shreya-jain-/",
@@ -67,7 +59,7 @@ const verticalHeads = [
   },
   {
     name: "VANSHIKA GUPTA",
-    image: Vanshika,
+    image: "Vanshika.jpg",
     position: "STRATEGIC RELATIONS HEAD",
     email: "mailto:vanshika.gupta.met20@iitbhu.ac.in",
     linkedin: "https://www.linkedin.com/in/vanshika-gupta-299001201",
@@ -75,7 +67,7 @@ const verticalHeads = [
   },
   {
     name: "DIVYANSH THAKRE",
-    image: Divyansh,
+    image: "Divyansh.jpg",
     position: "INNOVATION & INCUBATION HEAD",
     email: "mailto:divyansh.thakre.civ20@itbhu.ac.in",
     linkedin: "https://www.linkedin.com/in/divyanshthakre/",
@@ -83,112 +75,132 @@ const verticalHeads = [
   },
 ];
 export default function Team22(props) {
+  const [imageLinks, setImageLinks] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
+  useEffect(() => {
+    const fetchImageLinks = async () => {
+      const folderName = "Team2022";
+      const folderRef = ref(StorageDB, folderName);
+      try {
+        const listResult = await listAll(folderRef);
+        const imageUrls = await Promise.all(
+          listResult.items.map(async (itemRef) => {
+            const imageName = itemRef.name;
+            const url = await getDownloadURL(itemRef);
+            return { name: imageName, url };
+          })
+        );
+        setImageLinks(imageUrls);
+        setIsloading(false);
+      } catch (error) {
+        setIsloading(false);
+        console.error('Error getting image URLs:', error);
+      }
+    };
+
+    fetchImageLinks();
+  }, []);
+
   return (
-    <div
+    isLoading ? 
+      
+        <img
+       className="block mx-auto"
+          src="/loading.gif"
+          width="300"
+          alt="Loading..."
+          height="300"
+        />
+    : <div
       className={"container"}
       style={{
         margin: "80px auto",
       }}
     >
       <div className={"row"}>
-        <Accordion className="past-team">
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <h2
-              style={{
-                fontSize: "1.4rem",
-                textAlign: "center",
-                width: "100%",
-                fontWeight: "bold",
-              }}
-            >
-              TEAM OF 2022-23
-            </h2>
-          </AccordionSummary>
-          <div className="team-container">
-            {presidents.map((president, index) => {
-              return (
-                <div className="our-team" key={index}>
-                  <Image
-                    src={president.image}
-                    className="img-responsive"
-                    alt="team member"
-                  />
-                  <div className="team-content">
-                    <h3 className="name">{president.name}</h3>
-                    <span className="post">{president.post}</span>
-                    <div className="team-social">
-                      <a
-                        href={president.email}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <GrMail className="social-icons"></GrMail>
-                      </a>
-                      <a
-                        href={president.linkedin}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <FaLinkedin className="social-icons"></FaLinkedin>
-                      </a>
-                      <a
-                        href={president.twitter}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <FaTwitter className="social-icons"></FaTwitter>
-                      </a>
-                    </div>
+        <h2
+          style={{
+            fontSize: "1.4rem",
+            textAlign: "center",
+            width: "100%",
+            fontWeight: "bold",
+          }}
+        >
+          TEAM OF 2022-23
+        </h2>
+        <div className="team-container">
+          {presidents.map((president, index) => {
+            const imageUrl = (imageLinks.find(image=>image.name==president.image))|| '/path/to/default/image.png';  // Use a default image if URL is not available
+           console.log(imageUrl)
+            return (
+              <div className="our-team" key={index}>
+                <Image
+                  src={imageUrl.url}
+                  height={1600}
+                  width={1600}
+                  className="img-responsive img-contain"
+                  alt={president.name}
+                />
+                <div className="team-content">
+                  <h3 className="name">{president.name}</h3>
+                  <span className="post">{president.post}</span>
+                  <div className="team-social">
+                    <a
+                      href={president.email}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <GrMail className="social-icons" />
+                    </a>
+                    <a
+                      href={president.linkedin}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaLinkedin className="social-icons" />
+                    </a>
                   </div>
                 </div>
-              );
-            })}
-          </div>
-          <div className="team-container-1">
-            {verticalHeads.map((head, index) => {
-              return (
-                <div className="our-team" key={index}>
-                  <Image
-                    src={head.image}
-                    className="img-responsive"
-                    alt="team member"
-                  />
-                  <div className="team-content">
-                    <h3 className="name">{head.name}</h3>
-                    <span className="post">{head.position}</span>
-                    <div className="team-social">
-                      <a
-                        href={head.email}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <GrMail className="social-icons"></GrMail>
-                      </a>
-                      <a
-                        href={head.linkedin}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <FaLinkedin className="social-icons"></FaLinkedin>
-                      </a>
-                      <a
-                        href={head.twitter}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <FaTwitter className="social-icons"></FaTwitter>
-                      </a>
-                    </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="team-container-1">
+          {verticalHeads.map((head, index) => {
+            const imageUrl = (imageLinks.find(image=>image.name==head.image))  || '/path/to/default/image.png';  // Use a default image if URL is not available
+            return (
+              <div className="our-team" key={index}>
+                <Image
+                  src={imageUrl.url}
+                  height={1600}
+                  width={1600}
+                  className="img-responsive img-contain"
+                  alt={head.name}
+                />
+                <div className="team-content">
+                  <h3 className="name">{head.name.toUpperCase()}</h3>
+                  <span className="post">{head.position}</span>
+                  <div className="team-social">
+                    <a
+                      href={head.email}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <GrMail className="social-icons" />
+                    </a>
+                    <a
+                      href={head.linkedin}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaLinkedin className="social-icons" />
+                    </a>
                   </div>
                 </div>
-              );
-            })}
-          </div>
-        </Accordion>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
