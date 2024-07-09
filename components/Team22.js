2beca565eaaba61f2ea56862f9cfce11c0ab176c
Tridip Barman <tridipbarman09@gmail.com>
2024-07-10 02:39:37 +0530

    code structure imporved and added team21.js

--- components/Team22.js
+++ components/Team22.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/legacy/image";
 import { GrMail } from "react-icons/gr";
-import { FaLinkedin } from "react-icons/fa";
+import { FaLinkedin, FaTwitter } from "react-icons/fa";
 import { listAll, getDownloadURL, ref } from "firebase/storage";
 import { StorageDB } from "@/lib/firebase";
 import { team2022 } from "@/staticdata/TeamsData/Team2022";
@@ -34,106 +34,110 @@ export default function Team22(props) {
   }, []);
 
   return (
-    isLoading ? 
-      
-        <img
-       className="block mx-auto"
-          src="/loading.gif"
-          width="300"
-          alt="Loading..."
-          height="300"
-        />
-    : <div
-      className={"container"}
-      style={{
-        margin: "80px auto",
-      }}
-    >
-      <div className={"row"}>
-        <h2
-          style={{
-            fontSize: "1.4rem",
-            textAlign: "center",
-            width: "100%",
-            fontWeight: "bold",
-          }}
-        >
-          TEAM OF 2022-23
-        </h2>
-        <div className="team-container">
-          {team2022?.presidents.map((president, index) => {
-            const imageUrl = (imageLinks.find(image=>image.name==president.image))|| '/path/to/default/image.png';  // Use a default image if URL is not available
-           console.log(imageUrl)
-            return (
-              <div className="our-team" key={index}>
-                <Image
-                  src={imageUrl.url}
-                  height={1600}
-                  width={1600}
-                  className="img-responsive img-contain"
-                  alt={president.name}
-                />
-                <div className="team-content">
-                  <h3 className="name">{president.name}</h3>
-                  <span className="post">{president.post}</span>
-                  <div className="team-social">
-                    <a
-                      href={president.email}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <GrMail className="social-icons" />
-                    </a>
-                    <a
-                      href={president.linkedin}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaLinkedin className="social-icons" />
-                    </a>
+    isLoading ?
+
+      <img
+        className="block mx-auto"
+        src="/loading.gif"
+        width="300"
+        alt="Loading..."
+        height="300"
+      />
+      : <div
+        className={"container"}
+        style={{
+          margin: "80px auto",
+        }}
+      >
+        <div className={"row"}>
+          <div className="team-container">
+            {team2022?.presidents.map((president, index) => {
+              const imageUrl = (imageLinks.find(image => image.name == president.image)) || '/path/to/default/image.png';  // Use a default image if URL is not available
+              console.log(imageUrl)
+              return (
+                <div className="our-team" key={index}>
+                  <Image
+                    src={imageUrl.url}
+                    height={1600}
+                    width={1600}
+                    className="img-responsive img-contain"
+                    alt={president.name}
+                  />
+                  <div className="team-content">
+                    <h3 className="name">{president.name}</h3>
+                    <span className="post">{president.post}</span>
+                    <div className="team-social">
+                      <a
+                        href={president.email}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <GrMail className="social-icons" />
+                      </a>
+                      <a
+                        href={president.linkedin}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <FaLinkedin className="social-icons" />
+                      </a>
+                      <a
+                        href={president.twitter}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <FaTwitter className="social-icons" />
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="team-container-1">
-          {team2022.verticalHeads.map((head, index) => {
-            const imageUrl = (imageLinks.find(image=>image.name==head.image))  || '/path/to/default/image.png';  // Use a default image if URL is not available
-            return (
-              <div className="our-team" key={index}>
-                <Image
-                  src={imageUrl.url}
-                  height={1600}
-                  width={1600}
-                  className="img-responsive img-contain"
-                  alt={head.name}
-                />
-                <div className="team-content">
-                  <h3 className="name">{head.name.toUpperCase()}</h3>
-                  <span className="post">{head.position}</span>
-                  <div className="team-social">
-                    <a
-                      href={head.email}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <GrMail className="social-icons" />
-                    </a>
-                    <a
-                      href={head.linkedin}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaLinkedin className="social-icons" />
-                    </a>
+              );
+            })}
+          </div>
+          <div className="team-container-1">
+            {team2022.verticalHeads.map((head, index) => {
+              const imageUrl = (imageLinks.find(image => image.name == head.image)) || '/path/to/default/image.png';  // Use a default image if URL is not available
+              return (
+                <div className="our-team" key={index}>
+                  <Image
+                    src={imageUrl.url}
+                    height={1600}
+                    width={1600}
+                    className="img-responsive img-contain"
+                    alt={head.name}
+                  />
+                  <div className="team-content">
+                    <h3 className="name">{head.name.toUpperCase()}</h3>
+                    <span className="post">{head.position}</span>
+                    <div className="team-social">
+                      <a
+                        href={head.email}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <GrMail className="social-icons" />
+                      </a>
+                      <a
+                        href={head.linkedin}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <FaLinkedin className="social-icons" />
+                      </a>
+                      <a
+                        href={head.twitter}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <FaTwitter className="social-icons" />
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
   );
 }
--- pages/team.js
+++ pages/team.js
@@ -2,20 +2,11 @@ import React from "react";
 import Head from "next/head";
 import Footer from "../components/Footer";
 import Nav from "../components/navbar/NavLayout";
-import { FaLinkedin, FaTwitter } from "react-icons/fa";
-import { GrMail } from "react-icons/gr";
 import Image from "next/legacy/image";
-import akj from "../public/team/akj.jpg";
-import prof from "../public/team/rajnesh_tyagi.png";
-import vp from "../public/team/vp.jpg";
-import avp from "../public/team/amitavp.jpg";
-import sr from "../public/team/sr.jpeg";
-import rieu from "../public/team/rieu.jpeg";
-import ev from "../public/team/ev.jpeg";
-import wnb from "../public/team/wnb.jpg";
-import tqb from "../public/team/tqb.jpeg";
+import prof from "../public/rajnesh_tyagi.png";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Accordion, AccordionSummary } from "@mui/material";
+import Team21 from "@/components/Team21";
 import Team22 from "../components/Team22";
 import Team23 from "../components/Team23";
 import Team24 from "../components/Team24";
@@ -91,7 +82,25 @@ const Team = () => {
           </h2>
           <Team24 />
           <Team23 />
-          <Team22 />
+          <Accordion className="past-team">
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <h2
+                style={{
+                  fontSize: "1.4rem",
+                  textAlign: "center",
+                  width: "100%",
+                  fontWeight: "bold",
+                }}
+              >
+                TEAM OF 2022-23
+              </h2>
+            </AccordionSummary>
+            <Team22 />
+          </Accordion>
           <Accordion className="past-team">
             <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
@@ -109,254 +118,7 @@ const Team = () => {
                 TEAM OF 2021-22
               </h2>
             </AccordionSummary>
-            <div className="team-container">
-              <div className="our-team">
-                <Image src={vp} className="img-responsive" alt="team member" />
-                <div className="team-content">
-                  <h3 className="name">HARSHIT SHUKLA</h3>
-                  <span className="post">VICE PRESIDENT E-CELL</span>
-                  <div className="team-social">
-                    <a
-                      href={`https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=harshitshukla.eee18@iitbhu.ac.in`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <GrMail className="social-icons"></GrMail>
-                    </a>
-                    <a
-                      href="https://www.linkedin.com/in/harshitshuklaiitbhu/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaLinkedin className="social-icons"></FaLinkedin>
-                    </a>
-                    <a
-                      href="https://twitter.com/harshit_tw"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaTwitter className="social-icons"></FaTwitter>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="our-team">
-                <Image src={avp} className="img-responsive" alt="team member" />
-                <div className="team-content">
-                  <h3 className="name">AMIT KUMAR BAHETI</h3>
-                  <span className="post">ASSOCIATE VICE PRESIDENT E-CELL</span>
-                  <div className="team-social">
-                    <a
-                      href={`https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=amit.krbaheti.eee19@iitbhu.ac.in`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <GrMail className="social-icons"></GrMail>
-                    </a>
-                    <a
-                      href="https://www.linkedin.com/in/amitkumarbaheti/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaLinkedin className="social-icons"></FaLinkedin>
-                    </a>
-                    <a
-                      href="https://twitter.com/amit_baheti_"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaTwitter className="social-icons"></FaTwitter>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="team-container-1">
-              <div className="our-team">
-                <Image src={wnb} className="img-responsive" alt="team member" />
-                <div className="team-content">
-                  <h3 className="name">AATISHYA SOOD</h3>
-                  <span className="post">WEB AND BRANDING HEAD</span>
-                  <div className="team-social">
-                    <a
-                      href={`https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=aatishya.sood.met19@itbhu.ac.in`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <GrMail className="social-icons"></GrMail>
-                    </a>
-                    <a
-                      href="https://www.linkedin.com/in/aatishya-sood-5699081a7"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaLinkedin className="social-icons"></FaLinkedin>
-                    </a>
-                    <a
-                      href="https://twitter.com/aatishya?s=09"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaTwitter className="social-icons"></FaTwitter>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="our-team">
-                <Image src={akj} className="img-responsive" alt="team member" />
-                <div className="team-content">
-                  <h3 className="name">AMIT KUMAR JHA</h3>
-                  <span className="post">SAP HEAD</span>
-                  <div className="team-social">
-                    <a
-                      href={`https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=amit.kumarjha.met19@itbhu.ac.in`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <GrMail className="social-icons"></GrMail>
-                    </a>
-                    <a
-                      href="https://www.linkedin.com/in/amit-jha-96022618b"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaLinkedin className="social-icons"></FaLinkedin>
-                    </a>
-                    <a
-                      href="https://twitter.com/AmitKum17723914"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaTwitter className="social-icons"></FaTwitter>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="our-team  ">
-                <Image src={tqb} className="img-responsive" alt="team member" />
-                <div className="team-content">
-                  <h3 className="name">ANSHUL SINGH</h3>
-                  <span className="post">STUDENT BODY HEAD, CISCO THINGQ</span>
-                  <div className="team-social">
-                    <a
-                      href={`https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=anshul.singh.met19@itbhu.ac.in`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <GrMail className="social-icons"></GrMail>
-                    </a>
-                    <a
-                      href={`https://www.linkedin.com/in/anshul-singh-b58790197`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaLinkedin className="social-icons"></FaLinkedin>
-                    </a>
-                    <a
-                      href="https://twitter.com/AnshulS61107731"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaTwitter className="social-icons"></FaTwitter>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="our-team ">
-                <Image src={ev} className="img-responsive" alt="team member" />
-                <div className="team-content">
-                  <h3 className="name">ABHISHEK NAVNEET FARDE</h3>
-                  <span className="post">EVENTS HEAD</span>
-                  <div className="team-social">
-                    <a
-                      href={`https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=abhisheknfarde.mec18@itbhu.ac.in`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <GrMail className="social-icons"></GrMail>
-                    </a>
-                    <a
-                      href={`https://www.linkedin.com/in/abhishek-farde-95b871194`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaLinkedin className="social-icons"></FaLinkedin>
-                    </a>
-                    <a
-                      href={`https://twitter.com/AbhishekFarde?s=09`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaTwitter className="social-icons"></FaTwitter>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="our-team">
-                <Image src={sr} className="img-responsive" alt="team member" />
-                <div className="team-content">
-                  <h3 className="name">ANMOL GOYAL</h3>
-                  <span className="post">STRATEGIC RELATIONS HEAD</span>
-                  <div className="team-social">
-                    <a
-                      href={`https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=anmol.goyal.civ19@itbhu.ac.in`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <GrMail className="social-icons"></GrMail>
-                    </a>
-                    <a
-                      href="https://www.linkedin.com/in/anmol-goyal-324852194"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaLinkedin className="social-icons"></FaLinkedin>
-                    </a>
-                    <a
-                      href="https://twitter.com/AnmolGo37120796"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaTwitter className="social-icons"></FaTwitter>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="our-team  ">
-                <Image
-                  src={rieu}
-                  className="img-responsive"
-                  alt="team member"
-                />
-                <div className="team-content">
-                  <h3 className="name">ARSHAD KHAN</h3>
-                  <span className="post">RIEU HEAD</span>
-                  <div className="team-social">
-                    <a
-                      href={`https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=mdarshadkhan.bme18@itbhu.ac.in`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <GrMail className="social-icons"></GrMail>
-                    </a>
-                    <a
-                      href="https://www.linkedin.com/in/mohammed-arshad-khan-0b766a191"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaLinkedin className="social-icons"></FaLinkedin>
-                    </a>
-                    <a
-                      href="https://twitter.com/mak640477"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaTwitter className="social-icons"></FaTwitter>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Team21 />
           </Accordion>
         </div>
       </div>
