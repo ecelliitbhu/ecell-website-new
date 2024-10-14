26198629a1dff585dc554e4ff0d9e92f1e474551
Suhani-iitbhueee <suhani.singh.eee23@itbhu.ac.in>
2024-10-14 13:07:10 +0530

    prisma setup

---
+++ pages/api/CampusAmbass/routefirst.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+import prisma from '../../../lib/prismaConfig.js';
+
+//define route
+
+
+
+
+//exporting
+export default router;
---
+++ server.js
@@ -0,0 +1,29 @@
+// const express = require('express');
+// const app = express();
+// const dotenv = require('dotenv'); 
+// // Require routes
+// const routeFirst = require('./pages/api/campusAmbass/routefirst.js');
+
+
+
+
+
+
+// dotenv.config(); 
+// const PORT = process.env.PORT || 3000;
+// app.use(express.json());
+
+
+// // Use the defined routes
+// //app.use('/...',routeFirst);
+
+
+
+
+// app.get('/', (req, res) => {
+//     res.send('Hello, World!');
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
